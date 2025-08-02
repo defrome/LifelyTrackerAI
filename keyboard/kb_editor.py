f7857da965ba9e260baeee6e3f2c3769644707e6
defrome <iskoblov555@gmail.com>
2025-08-02 15:44:58 +0300

    log indicators keyboard added, main file edited

--- keyboard/kb_editor.py
+++ keyboard/kb_editor.py
@@ -3,11 +3,25 @@ from aiogram.utils.keyboard import (
     InlineKeyboardBuilder
 )
 
+
 def main_menu_kb():
-    builder = ReplyKeyboardBuilder()
-    builder.button(text="📝 Записать показатели")
-    builder.button(text="📊 Статистика")
-    builder.button(text="⚙️ Настройки")
-    builder.button(text="ℹ️ Помощь")
-    builder.adjust(2)
-    return builder.as_markup(resize_keyboard=True, one_time_keyboard=False)
+    builder = InlineKeyboardBuilder()
+
+    builder.button(text="📝 Записать показатели", callback_data="log_indicators")
+    builder.button(text="📊 Статистика", callback_data="show_stats")
+    builder.button(text="⚙️ Настройки", callback_data="settings")
+    builder.button(text="ℹ️ Помощь", callback_data="help")
+
+    builder.adjust(1, 2, 1)  # 2 кнопки в первом ряду, остальные во втором
+    return builder.as_markup()
+
+def log_indicators_kb():
+    builder = InlineKeyboardBuilder()
+
+    builder.button(text="Записать прием воды", callback_data="water_ind")
+    builder.button(text="Записать прием пищи", callback_data="eat_ind")
+    builder.button(text="Записать весовые и ростовые показатели", callback_data="ves_and_rost_ind")
+    builder.button(text="Помощь искусственного интелекта", callback_data="ai_help")
+
+    builder.adjust(1, 2, 1)  # 2 кнопки в первом ряду, остальные во втором
+    return builder.as_markup()
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from aiogram.types import Message
 import requests
 import asyncio
 
-from keyboard.kb_editor import main_menu_kb
+from keyboard.kb_editor import main_menu_kb, log_indicators_kb
 
 BOT_TOKEN='8124788304:AAGbT5IlXTC15WeuMz-1f2PgHeCq1oo5tEw'
 
@@ -30,6 +30,10 @@ async def send_welcome(message: types.message):
 Выбери действие в меню ниже 👇
     """, reply_markup=main_menu_kb())
 
+
+@dp.callback_query(F.data == "log_indicators")
+async def log_indicators(message: types.message):
+    await message.reply(f"Test", reply_markup=log_indicators_kb())
 
 
 async def main():
